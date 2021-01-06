344e3c81753305a2819e8aeec3e98fffbc76ca04
Muhammad Abdullah <kb337137@gmail.com>
2021-01-07 02:47:45 +0500

    changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 5000;
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Bycrypt = require("bcrypt-inzi");
 const useragent = require('express-useragent');
@@ -10,11 +10,101 @@ const useragent = require('express-useragent');
 let app = express();
 
 app.use(useragent.express());
-
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 let dbURI = "mongodb+srv://root:root@cluster0.s5oku.mongodb.net/testdb?retryWrites=true&w=majority"
 
-mongoose.connect
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on("connected", function () {
+    console.log("Mongoose is Connected");
+});
+
+mongoose.connection.on("disconnected", function () {
+    console.log("Mogoose is disconnected");
+});
+
+mongoose.connection.on("err", function (err) {
+    console.log("Mongoose connection error" + err);
+    process.exit(1);
+});
+
+process.on("SIGINT", function () {
+    console.log("app is terminated");
+    mongoose.connection.close(function () {
+        console.log("Mongoose Default Connection Close");
+    });
+});
+
+mongoose.set('useCreateIndex', true);
+var userSchema = new mongoose.Schema({
+    userName: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+var users = mongoose.model("users", userSchema);
+
+var sessionScheme = new mongoose.Schema({
+    token: { type: String },
+    expire: { type: String }
+});
+
+var sessions = mongoose.model("sessions", sessionScheme);
+
+app.get("/", (req, res, next) => {
+    res.send("Some GET ANYONE");
+});
+
+app.post("/signup", (req, res, next) => {
+
+    if (!req.body.userName || !req.body.password) {
+        console.log("Something Missing");
+        res.send({
+            message: "Please Provide Username Password"
+        });
+    }
+    Bycrypt.stringToHash(JSON.stringify(req.body.password)).then(passwordHash => {
+        var newUser = new users({
+            userName: req.body.userName,
+            password: passwordHash
+        })
+        newUser.save((err, data) => {
+            if (!err) {
+                res.send({
+                    message: "Sign UP SuccessFully",
+                    status: 200
+                })
+            }
+            else{
+
+                res.send({
+                    message: "User Error "+err,
+                    status: 200
+                })
+                
+            }
+        })
+
+    })
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
